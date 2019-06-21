e461f6404ae8f7121cda83411abe86a17062e272
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-06-20 21:18:08 -0400

    done with all functionality

--- wedding_website_frontend/src/components/Rsvp.js
+++ wedding_website_frontend/src/components/Rsvp.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Guestlist from './Guestlist'
 import Guest from './Guest'
 import GuestRsvp from './GuestRSVP'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 // const baseAPI = 'https://radiant-eyrie-37659.herokuapp.com/'
 const baseAPI = 'http://localhost:3000/'
@@ -10,7 +11,7 @@ class Rsvp extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentUser: '',
+      current_user: '',
       id: '',
       first_name: '',
       last_name: '',
@@ -20,8 +21,9 @@ class Rsvp extends Component {
       zip: '',
       attending: '',
       guests: [],
-      guest: [],
-      showRsvp: false
+      showRsvp: false,
+      bride_name: '',
+      bride_lname: ''
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -33,17 +35,34 @@ class Rsvp extends Component {
     this.handleRadioButton = this.handleRadioButton.bind(this)
     this.handleSubmitRsvp = this.handleSubmitRsvp.bind(this)
     this.fetchGuest = this.fetchGuest.bind(this)
+    this.handleGuestRSVP = this.handleGuestRSVP.bind(this)
+    this.handleSubmitBride = this.handleSubmitBride.bind(this)
+    this.handleChangeBride = this.handleChangeBride.bind(this)
   }
 
   handleSubmit(event) {
     event.preventDefault()
     this.handleRsvp()
-    this.fetchGuests()
-    console.log('this is from handleSubmit', this.state);
+    this.fetchGuest(this.state.id)
+  }
 
+  handleSubmitBride(event) {
+    console.log(this.state);
+    event.preventDefault()
+    this.handleRsvp()
+    this.fetchGuests()
+    this.setState({
+      current_user: this.state.current_user
+    })
   }
 
   handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  handleChangeBride(event) {
     this.setState({
       [event.target.id]: event.target.value
     })
@@ -80,9 +99,15 @@ class Rsvp extends Component {
     fetch(baseAPI + `guests/` + `${guestId}`)
         .then( data => data.json())
         .then( guestData => {
-          console.log('this is guest data', guestData);
             this.setState({
-              guest: guestData
+              id: guestData.id,
+              first_name: guestData.first_name,
+              last_name: guestData.last_name,
+              address: guestData.address,
+              city: guestData.city,
+              state: guestData.state,
+              zip: guestData.zip,
+              attending: guestData.attending
             })
         })
   }
@@ -129,22 +154,45 @@ class Rsvp extends Component {
     })
   }
 
+  handleGuestRSVP(guestId) {
+    fetch(baseAPI + `guests/` + `${this.state.id}`, {
+      body: JSON.stringify({
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        address: this.state.address,
+        city: this.state.city,
+        state: this.state.state,
+        zip: this.state.zip,
+        attending: this.state.attending
+      }),
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then( updatedGuest => updatedGuest.json())
+    .then( updatedJData => {
+      this.updateArray(updatedJData, 'guests')
+    })
+    .catch( err => console.log('this is err', err))
+  }
+
   handleRadioButton(value) {
     this.setState({
       attending: value,
     })
-    console.log(this.state);
   }
 
   handleSubmitRsvp(e) {
     e.preventDefault()
     this.handleRadioButton()
-    this.handleEditGuest()
-    console.log(this.state);
+    this.handleGuestRSVP(this.state.id)
+    this.props.history.push('/')
+    alert("Thank you for your RSVP!")
   }
 
   render() {
-    console.log(this.state);
     return (
       <React.Fragment>
         <div className="rsvp">
@@ -152,8 +200,9 @@ class Rsvp extends Component {
           <h1 className="cursive heading">RSVP</h1>
           <img className="eucalyptus" src="https://png2.kisspng.com/sh/283511c8f211fc299e7eacdaec04f5f7/L0KzQYm3U8I5N6dtfZH0aYP2gLBuTgdmbJVuhtk2aX75ecXolPlwdl5nitttZYPwcbrrTfV2a5JxkeJ9dYOwd8b1jvlqNZ5mRddAY3HvicH7lgMuPZJneqduM3XmdLXpWcYvQGM9TasAM0W0RYO5UsI5P2o9UKY8Mj7zfri=/kisspng-wedding-invitation-bridesmaid-eucalyptus-gunnii-ma-eucalyptus-5abb5e3ecddb96.8285953515222287988432.png" />
           <h3>To RSVP for our wedding, log in using your first name, last name, & RSVP Code found on your invitation.</h3><br />
-
+          <div className="forms">
             <form onSubmit={this.handleSubmit}>
+              Guest RSVP:< br/>
               First Name:<input
                   type="text"
                   id="first_name"
@@ -174,10 +223,34 @@ class Rsvp extends Component {
                   value="I Do." />
             </form>
 
+            <form onSubmit={this.handleSubmitBride}>
+              Bride & Groom Login:< br/>
+              First Name:<input
+                  type="text"
+                  id="bride_name"
+                  value={this.state.bride_name}
+                  onChange={this.handleChangeBride} /><br />
+              Last Name:<input
+                  type="text"
+                  id="bride_lname"
+                  value={this.state.bride_lname}
+                  onChange={this.handleChangeBride} /><br />
+              Password:<input
+                  type="password"
+                  id="current_user"
+                  value={this.state.current_user}
+                  onChange={this.handleChangeBride} /><br />
+              <input
+                  type="submit"
+                  value="I Do." />
+            </form>
+
+          </div>
 
             { (this.state.showRsvp) ?
-               (this.state.id === 'charlie') ?
+               (this.state.current_user === 'charlie')
 
+               ?
 
               <div>
                 <h1>Welcome to your Guestlist Management!</h1>
@@ -193,6 +266,8 @@ class Rsvp extends Component {
                     handleChange={this.handleChange}
                     handleDeleteGuest={this.handleDeleteGuest}
                     updateArray={this.updateArray}
+                    handleSubmitBride={this.handleSubmitBride}
+                    handleChangeBride={this.handleChangeBride}
                 />
               </div>
 
@@ -209,6 +284,7 @@ class Rsvp extends Component {
                   handleEditGuest={this.handleEditGuest}
                   guest={this.state.guest}
                   fetchGuests={this.fetchGuests}
+                  handleGuestRSVP={this.handleGuestRSVP}
               />
 
             : ''
