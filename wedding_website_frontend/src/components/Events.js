9716e3e077397e33c2e24a33283f53b95e95050e
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-06-23 20:57:45 -0400

    final commit

--- wedding_website_frontend/src/components/Events.js
+++ wedding_website_frontend/src/components/Events.js
@@ -8,13 +8,13 @@ class Events extends Component {
             <h1 className="cursive heading">Eat, Drink, and be Married</h1>
             <img className="eucalyptus" src="https://png2.kisspng.com/sh/283511c8f211fc299e7eacdaec04f5f7/L0KzQYm3U8I5N6dtfZH0aYP2gLBuTgdmbJVuhtk2aX75ecXolPlwdl5nitttZYPwcbrrTfV2a5JxkeJ9dYOwd8b1jvlqNZ5mRddAY3HvicH7lgMuPZJneqduM3XmdLXpWcYvQGM9TasAM0W0RYO5UsI5P2o9UKY8Mj7zfri=/kisspng-wedding-invitation-bridesmaid-eucalyptus-gunnii-ma-eucalyptus-5abb5e3ecddb96.8285953515222287988432.png" />
             <h3>Rehersal Dinner</h3>
-            <span>Edgewood Country Club</span>
+            <span>Edgewood Country Club | 6:30pm - 10pm</span>
             <h3>Wedding Ceremony</h3>
-            <span>Saint Agnes Catholic Church</span>
+            <span>Saint Agnes Catholic Church | 2:00pm - 4:00pm</span>
             <h3>Reception</h3>
-            <h4>Cocktail Hour</h4>
-            <h4>Dinner</h4>
             <span>Charleston Civic Center</span>
+            <span>Cocktail Hour | 4:30pm - 6:00pm</span>
+            <span>Dinner | 6:30pm - 7:30pm</span>
           </div>
         </div>
     )
